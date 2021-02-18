afff82f9e46c362b280c0b15cbf5f4f032f7ac20
Kayla Melton <kannmelton2@gmail.com>
2021-02-18 10:07:35 -0600

    completed the Beginner Lost Without A Map Challenge on codewars

---
+++ beginnerlostwithoutamap.js
@@ -0,0 +1,17 @@
+/*
+Prompt:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+*/
+
+const double = (arr) => arr.map((x) => x*2);
+
+/*
+Notes:
+They gave away the solution with the hint for using the map method, I will not refactor.
+*/

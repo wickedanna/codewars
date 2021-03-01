48a74be2158a95976374dc7a3f1ec40ca63ad926
Kayla Melton <kannmelton2@gmail.com>
2021-02-28 22:51:46 -0600

    solved separate basic types challenge on codewars

---
+++ separatebasictypes.js
@@ -0,0 +1,28 @@
+/*
+Prompt:
+Given: a sequence of different type of values (number, string, boolean).
+You should return an object with a separate properties for each of types presented in input.
+Each property should contain an array of corresponding values.
+
+keep order of values like in input array
+if type is not presented in input, no corresponding property are expected
+So, for this input:
+
+['a', 1, 2, false, 'b']
+expected output is:
+
+{
+  number: [1, 2],
+  string: ['a', 'b'],
+  boolean: [false]
+}
+*/
+
+const separateTypes = arr => {
+  return arr.reduce((types, value) => {
+    const valueType = typeof value
+    const type = types[valueType]
+    types[valueType] = type ? [...type, value] : [value]
+    return types
+  }, {})
+}
